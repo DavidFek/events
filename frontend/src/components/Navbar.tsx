ef7ff7b0816a0fc0b7b9b6c7c2f24e6252c21ec1
DavidF <davidfekete39@gmail.com>
2024-07-24 12:43:25 +0200

    changed backend dockerfile added mainpage to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import calendarImage from "../assets/calendar.png";
 import userImage from "../assets/user.png";
 import writerImage from "../assets/writer.png";
 import editImage from "../assets/edit.png";
+import mainPageImage from "../assets/mainpage.png";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -17,6 +18,15 @@ const Navbar = () => {
   return (
     <nav className="flex flex-col h-screen border-r-2 border-gray-600 w-64 bg-gray-900 shadow-2xl">
       <ul className="flex flex-col h-full">
+        <li className="border-b-2 border-gray-600">
+          <Link to="/" className="p-4 block hover:bg-gray-600">
+            <div className="flex items-center space-x-2">
+              {" "}
+              <img src={mainPageImage} alt="calendar" className="w-6 h-6" />
+              <span>Main Page</span>{" "}
+            </div>
+          </Link>
+        </li>
         <li className="border-b-2 border-gray-600">
           <Link to="/events" className="p-4 block hover:bg-gray-600">
             <div className="flex items-center space-x-2">
